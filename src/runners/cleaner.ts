1d31585c8d16260bcf149ad0cc299947b5459606
nybble <nybble@yandex-team.ru>
2022-01-13 08:17:42 +0300

    add error handling

--- src/runners/cleaner.ts
+++ src/runners/cleaner.ts
@@ -11,35 +11,39 @@ export const cleaner: HostRunner = task => async host => {
     const bashDiskUsage = getDiskUsage(bash);
     const bashRemove = mode === 'real' ? removeRecByPath(bash) : I;
     console.log('Gathering paths...');
-    const paths = await sniffer({host});
     const write = reportWrite({task, folder: host});
-    if (!paths.length) {
-        const nothingMsg = 'nothing to delete';
-        console.log(nothingMsg);
-        report.push(nothingMsg);
-        return write(report.join('\n'));
-    }
-    console.log('Deleting...');
-    const duBefore = await bashDiskUsage();
-    await mapAsync(async (path: string) => {
-        try {
-            process.stdout.write(`deleting: ${path}`);
-            if (/\/~$/.test(path)) await bashRemove('*~*', path.split('/').slice(0, -1).join('/'));
-            await bashRemove(path, '/');
-            report.push(`deleted: ${path}`);
-            process.stdout.write(' - done!\n');
-        } catch (err: any) {
-            report.push(err);
-        }
-    })(paths);
-
-    report.push('DISK USAGE BEFORE:', duBefore);
-    report.push('DISK USAGE AFTER:', await bashDiskUsage());
-
     try {
-        await write(report.join('\n'));
-    } catch (err) {
-        console.log(err);
+        const paths = await sniffer({host});
+        if (!paths.length) {
+            const nothingMsg = 'nothing to delete';
+            console.log(nothingMsg);
+            report.push(nothingMsg);
+            return write(report.join('\n'));
+        }
+        console.log('Deleting...');
+        const duBefore = await bashDiskUsage();
+        await mapAsync(async (path: string) => {
+            try {
+                process.stdout.write(`deleting: ${path}`);
+                if (/\/~$/.test(path)) await bashRemove('*~*', path.split('/').slice(0, -1).join('/'));
+                await bashRemove(path, '/');
+                report.push(`deleted: ${path}`);
+                process.stdout.write(' - done!\n');
+            } catch (err: any) {
+                report.push(err);
+            }
+        })(paths);
+
+        report.push('DISK USAGE BEFORE:', duBefore);
+        report.push('DISK USAGE AFTER:', await bashDiskUsage());
+
+        try {
+            await write(report.join('\n'));
+        } catch (err) {
+            console.log(err);
+        }
+    } catch (err: any) {
+        await write(err.message);
     }
     return undefined;
 };
